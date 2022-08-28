c9cfacd41c54b27f9824bcb7112db0a19b704322
Limo Kiplimo <limo@mega.com>
2022-08-28 10:49:15 +0300

    initial commit

---
+++ users/views.py
@@ -0,0 +1,70 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
+from users.models import User
+from .serializers import UserSerializer
+import jwt, datetime
+
+
+# Create your views here.
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+class LoginView(APIView):
+    def post(self,request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            raise AuthenticationFailed('User not found')
+
+        if not user.check_password(password):
+            raise AuthenticationFailed('Incorrect Password')
+
+        #Generate JWT for Authentication
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.utcnow()
+        }
+        token = jwt.encode(payload, 'secret', algorithm='HS256')
+
+        #Send token via cookies
+        response=Response()
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.data = {'jwt':token}
+        return response
+        
+
+        
+class UserView(APIView):
+    def get(self,request):
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            raise AuthenticationFailed('Unauthenticated')
+        
+        try:
+            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Login token is expired')
+
+        user=User.objects.filter(id=payload['id']).first()
+        serializer=UserSerializer(user)
+        return Response (serializer.data)
+
+
+
+class LogoutView(APIView):
+    def post(self, request):
+        response=Response()
+        response.delete_cookie('AuthToken')
+        response.data = {'message':  'success'}
+        return response
